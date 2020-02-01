7b321baf9fb72124c270f4f5cf9a376e033fa9bf
deeper-x <albertodeprezzo@gmail.com>
2020-02-01 01:24:46 +0100

    refactoring: created routing package for separation of concerns

--- main.go
+++ main.go
@@ -2,85 +2,36 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 
-	"github.com/gin-gonic/gin"
-	"github.com/graphql-go/graphql"
-
 	"net/http"
+
+	"github.com/deeper-x/gomparator/routing"
+	"github.com/gin-gonic/gin"
 )
 
-type query struct{}
-
-func (q *query) Hello() string {
-	return "hello!"
-}
+var result map[string]map[string]interface{}
 
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	router.Static("/js", "./js")
 
-	fields := graphql.Fields{
-		"hello": &graphql.Field{
-			Type: graphql.String,
-			Args: graphql.FieldConfigArgument{
-				"id": &graphql.ArgumentConfig{
-					Type: graphql.NewNonNull(graphql.Int),
-				},
-			},
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				// curl -i http://127.0.0.1:8000/?query={hello(id:100)}
-				return fmt.Sprintf("Hello! passed id is: %d", p.Args["id"]), nil
-			},
-		},
-		"foo": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "And content of foo is...", nil
-			},
-		},
-		"bar": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "Oh yeah, content of bar is...", nil
-			},
-		},
-	}
-
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
-
-	schema, err := graphql.NewSchema(schemaConfig)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	router.GET("/", func(c *gin.Context) {
-		param := c.Request.URL.Query()
+		query := c.Request.URL.Query()
 
-		query := param["query"][0]
+		res := routing.GetResult(query, routing.Schema)
 
-		params := graphql.Params{Schema: schema, RequestString: query}
+		data, _ := json.Marshal(res)
 
-		r := graphql.Do(params)
-
-		data, _ := json.Marshal(r)
-
-		var ro map[string]map[string]interface{}
-		err = json.Unmarshal(data, &ro)
+		err := json.Unmarshal(data, &result)
 
 		if err != nil {
 			log.Println(err)
 		}
 
-		res := ro["data"]
-
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"content": res,
+			"content": result["data"],
 		})
 	})
 
